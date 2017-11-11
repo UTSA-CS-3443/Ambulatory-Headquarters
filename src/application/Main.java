f53f13442ac053924cef6ede8ef52bc74f0703f3
Ryan Chen <chenjunhua1997@gmail.com>
2017-11-11 11:41:09 -0600

    Edited Main.java
    Edited Main.java to show MainGame.fxml view on start

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,18 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.*;
+import javafx.scene.*;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("view/MainGame.fxml"));
+			Scene scene = new Scene(root,1600,900);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
