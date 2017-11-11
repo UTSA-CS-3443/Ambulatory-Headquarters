036e18dde50b7c67960c7616d18ed42f1b703fcf
Ryan Chen <chenjunhua1997@gmail.com>
2017-11-11 11:34:36 -0600

    Added MainController.java

---
+++ src/application/controller/MainController.java
@@ -0,0 +1,24 @@
+package application.controller;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class MainController  implements EventHandler<ActionEvent>
+{
+	@FXML
+	private Label turnCountLb;
+	private Label notificationsLb;
+	
+	public MainController()
+	{
+		super();
+	}
+	@Override
+	public void handle(ActionEvent arg0) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
