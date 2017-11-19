0bbd7269a96def2f882777e0a0afb18d54b18056
chris_000 <chris_000@172.24.56.57>
2017-11-19 13:55:56 -0600

    Fixed char select

--- src/application/controller/CharacterSelectController.java
+++ src/application/controller/CharacterSelectController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import application.Main;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -8,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import units.UAlien;
 
 public class CharacterSelectController implements EventHandler<ActionEvent> 
 {
@@ -16,7 +18,7 @@ public class CharacterSelectController implements EventHandler<ActionEvent>
 	@FXML
 	CheckBox angelCb;
 	@FXML
-	CheckBox babeDragonCb;
+	CheckBox babyDragonCb;
 	@FXML
 	CheckBox cakeCb;
 	@FXML
@@ -54,6 +56,11 @@ public class CharacterSelectController implements EventHandler<ActionEvent>
 		Button b = (Button)event.getSource();
 		String text = b.getText();
 		
+		int unitCount = 0;
+		
+		int[] charS ={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		
+		
 		// sends game back to MainMenu.fxml
 		if (text.equals("<--Back")) 
 		{
@@ -68,11 +75,162 @@ public class CharacterSelectController implements EventHandler<ActionEvent>
 				e.printStackTrace();
 			}
 		}
-				
+		//Alien
+		if (alienCb.isSelected()) {
+			unitCount++;
+			charS[0] = 1;
+			System.out.println("ALIEN"+unitCount);
+		}
+		else if (!alienCb.isSelected()) {
+			charS[0] = 0;
+		}
+		
+		//Angel
+		if (angelCb.isSelected()) {
+			unitCount++;
+			charS[1] = 1;
+			System.out.println("ANGEL"+unitCount);
+		}
+		else if (!angelCb.isSelected()) {
+			charS[1] = 0;
+		}
+		
+		//Baby Dragon
+		if (babyDragonCb.isSelected()) {
+			unitCount++;
+			charS[2]=1;
+			System.out.println("BABEDRAGON"+unitCount);
+		}
+		else if (!babyDragonCb.isSelected()) {
+			charS[2] = 0;
+		}
+		
+		//Cake
+		if (cakeCb.isSelected()) {
+			unitCount++;
+			charS[3]=1;
+			System.out.println("CAKE"+unitCount);
+		}
+		else if (!cakeCb.isSelected()) {
+			charS[3] = 0;
+		}
+		
+		//Cat Burglar
+		if (catBurglarCb.isSelected()) {
+			unitCount++;
+			charS[4]=1;
+			System.out.println("CATBURGLAR"+unitCount);
+		}
+		else if (!catBurglarCb.isSelected()) {
+			charS[4] = 0;
+		}
+		
+		//Elf
+		if (elfCb.isSelected()) {
+			unitCount++;
+			charS[5]=1;
+			System.out.println("ELF"+unitCount);
+		}
+		else if (!elfCb.isSelected()) {
+			charS[5] = 0;
+		}
+		
+		//Ghost
+		if (ghostCb.isSelected()) {
+			unitCount++;
+			charS[6]=1;
+			System.out.println("GHOST"+unitCount);
+		}
+		else if (!ghostCb.isSelected()) {
+			charS[6] = 0;
+		}
+		
+		//Knight
+		if (knightCb.isSelected()) {
+			unitCount++;
+			charS[7]=1;
+			System.out.println("KNIGHT"+unitCount);
+		}
+		else if (!knightCb.isSelected()) {
+			charS[7] = 0;
+		}
+		
+		//Lunar Octopus..whatever that is
+		if (lunarOctopusCb.isSelected()) {
+			unitCount++;
+			charS[8]=1;
+			System.out.println("LUNAR OCTOPUS"+unitCount);
+		}
+		else if (!lunarOctopusCb.isSelected()) {
+			charS[8] = 0;
+		}
+		
+		//Ninja
+		if (ninjaCb.isSelected()) {
+			unitCount++;
+			charS[9]=1;
+			System.out.println("NINJA"+unitCount);
+		}
+		else if (!ninjaCb.isSelected()) {
+			charS[9] = 0;
+		}
+		
+		//Orc
+		if (orcCb.isSelected()) {
+			unitCount++;
+			charS[10]=1;
+			System.out.println("ORC"+unitCount);
+		}
+		else if (!orcCb.isSelected()) {
+			charS[10] = 0;
+		}
+		
+		//Pirate
+		if (pirateCb.isSelected()) {
+			unitCount++;
+			charS[11]=1;
+			System.out.println("PIRATE"+unitCount);
+		}
+		else if (!pirateCb.isSelected()) {
+			charS[11] = 0;
+		}
+		
+		//Space Cowboy
+		if (spaceCowboyCb.isSelected()) {
+			unitCount++;
+			charS[12]=1;
+			System.out.println("SPACECOWBOY"+unitCount);
+		}
+		else if (!spaceCowboyCb.isSelected()) {
+			charS[12] = 0;
+		}
+		
+		//Teddy Bear
+		if (teddyBearCb.isSelected()) {
+			unitCount++;
+			charS[13]=1;
+			System.out.println("TEDDYBEAR"+unitCount);
+		}
+		else if (!teddyBearCb.isSelected()) {
+			charS[13] = 0;
+		}
+		//Undead
+		if (undeadCb.isSelected()) {
+			unitCount++;
+			charS[14]=1;
+			System.out.println("UNDEAD"+unitCount);
+		}
+		else if (!undeadCb.isSelected()) {
+			charS[14] = 0;
+		}
+		
+		
 		// starts game by loading MainGame.fxml
-		if (text.equals("START!")) 
-		{
-			System.out.println("START!!");
+		if (text.equals("START!") && unitCount ==3) {
+			for (int i=0; i<15; i++) {
+				System.out.printf("%d", charS[i]);
+			}
+			System.out.println("\nSTART!!");
 			try {
 				Parent root = FXMLLoader.load(getClass().getResource("../view/MainGame.fxml"));
 				Scene scene = new Scene(root,1280,720);
@@ -82,6 +240,9 @@ public class CharacterSelectController implements EventHandler<ActionEvent>
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
+		}
+		else if (text.equals("START!") && (unitCount < 3 || unitCount > 3)) {
+			System.out.println("ONLY SELECT 3 UNITS");
 		}
 	}
 
