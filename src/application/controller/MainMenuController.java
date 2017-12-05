45546c5648578c6176b29e7a8fc1ffffb66d1e62
vwp914 <christopherotten2@gmail.com>
2017-12-05 10:57:07 -0600

    More JavaDocs

--- src/application/controller/MainMenuController.java
+++ src/application/controller/MainMenuController.java
@@ -11,72 +11,92 @@ import javafx.scene.input.MouseEvent;
 import javafx.beans.binding.*;
 import javafx.collections.*;
 
-public class MainMenuController  implements EventHandler<ActionEvent> 
-{
+/**
+ * This Class handles difficulty selection for the MainMenu.fxml. It also allows
+ * for the loading of the next view
+ * 
+ * @author Classic Cannon
+ *
+ */
+public class MainMenuController implements EventHandler<ActionEvent> {
 	@FXML
 	private Label turnCountLb;
 	private Label notificationsLb;
-	//
+
+	/**
+	 * Easy difficulty
+	 */
 	public static boolean easyDiff;
+
+	/**
+	 * Medium difficulty
+	 */
 	public static boolean mediumDiff;
+
+	/**
+	 * Hard difficulty
+	 */
 	public static boolean hardDiff;
-	
-	
-	
-	public MainMenuController() 
-	{
+
+	/**
+	 * Constructor to initialize all difficulties to false
+	 */
+	public MainMenuController() {
 		super();
-		easyDiff =  false;
+		easyDiff = false;
 		mediumDiff = false;
 		hardDiff = false;
 	}
-	
-	
+
 	@Override
-	public void handle(ActionEvent event)
-	{
-		Button b = (Button)event.getSource();
+	/**
+	 * Handles selection of difficulty and loading of next view
+	 */
+	public void handle(ActionEvent event) {
+		Button b = (Button) event.getSource();
 		String text = b.getText();
-		
-		//choose difficulty by clicking buttons and altering booleans
-				if (text.equals("EASY")) {
-					easyDiff = true;
-					mediumDiff = false;
-					hardDiff = false;
-					System.out.println("EASY!!");
-				}
-				
-				if (text.equals("MEDIUM")) {
-					mediumDiff = true;
-					easyDiff = false;
-					hardDiff = false;
-					System.out.println("MEDIUM!!");
-				}
-				
-				if (text.equals("HARD")) {
-					hardDiff = true;
-					easyDiff = false;
-					mediumDiff = false;
-					System.out.println("HARD!!");
-				}
-				
-				// exits game
-				if (text.equals("EXIT")) {
-					Main.stage.close();
-					System.out.println("Exit");
-				}
-				
-				// loads MenuCharacterSelect.fxml only after user selects a difficulty and clicks load
-				if (text.equals("LOAD") && (easyDiff == true || mediumDiff == true || hardDiff == true)) {
-					try {
-						// change over to a second view
-						Parent root = FXMLLoader.load(getClass().getResource("../view/MenuCharacterSelect.fxml"));   // Load the FXML
-						Main.stage.setScene(new Scene(root, 1280, 720));							   // Add the scene to the stage
-						Main.stage.show();														   // Show the stage to the user
-					}catch(Exception e) {
-						e.printStackTrace(); // TODO: app should do something more productive if errors occur...
-					}
-				}
+
+		// choose difficulty by clicking buttons and altering booleans
+		if (text.equals("EASY")) {
+			easyDiff = true;
+			mediumDiff = false;
+			hardDiff = false;
+			System.out.println("EASY!!");
+		}
+
+		if (text.equals("MEDIUM")) {
+			mediumDiff = true;
+			easyDiff = false;
+			hardDiff = false;
+			System.out.println("MEDIUM!!");
+		}
+
+		if (text.equals("HARD")) {
+			hardDiff = true;
+			easyDiff = false;
+			mediumDiff = false;
+			System.out.println("HARD!!");
+		}
+
+		// exits game
+		if (text.equals("EXIT")) {
+			Main.stage.close();
+			System.out.println("Exit");
+		}
+
+		// loads MenuCharacterSelect.fxml only after user selects a difficulty and
+		// clicks load
+		if (text.equals("LOAD") && (easyDiff == true || mediumDiff == true || hardDiff == true)) {
+			try {
+				// change over to a second view
+				Parent root = FXMLLoader.load(getClass().getResource("../view/MenuCharacterSelect.fxml")); // Load the
+																											// FXML
+				Main.stage.setScene(new Scene(root, 1280, 720)); // Add the scene to the stage
+				Main.stage.show(); // Show the stage to the user
+			} catch (Exception e) {
+				e.printStackTrace(); // TODO: app should do something more productive if errors occur...
+			}
+		}
 	}
 
 }
--- src/map/Map.java
+++ src/map/Map.java
@@ -9,175 +9,168 @@ import application.controller.MainMenuController;
 import units.*;
 
 /**
+ * This class handles the processing of each level, including loading the level
+ * and spawning entities. It also handles movement.
  * 
  * @author Classic Cannon
  *
  */
-public class Map 
-{
+public class Map {
 	private Unit[][] map;
 	private int maxEnemies = 15;
 	public Location firstTrue;
 	public Location lastTrue;
-	
+
 	/**
 	 * Map constructor
 	 */
-	public Map()
-	{
+	public Map() {
 		map = new Unit[12][16];
 		loadMap(1);
 	}
-	
+
 	/**
 	 * 
 	 * @return the map matrix of Units
 	 */
-	public Unit[][] getMatrix()
-	{
+	public Unit[][] getMatrix() {
 		return map;
 	}
-	
+
 	/**
 	 * 
-	 * @param r row
-	 * @param c column
+	 * @param r
+	 *            row
+	 * @param c
+	 *            column
 	 * @return the Unit at (r,c)
 	 */
-	public Unit get(int r, int c)
-	{
-		if(r < 0 || c < 0)
+	public Unit get(int r, int c) {
+		if (r < 0 || c < 0)
 			return null;
-		else if(r > map.length || c > map[0].length)
+		else if (r > map.length || c > map[0].length)
 			return null;
 		else
 			return map[r][c];
 	}
-	
+
 	/**
 	 * removes the Unit at (r,c)
-	 * @param r row
-	 * @param c column
+	 * 
+	 * @param r
+	 *            row
+	 * @param c
+	 *            column
 	 */
-	public void remove(int r, int c)
-	{
+	public void remove(int r, int c) {
 		map[r][c] = null;
 	}
-	
+
 	/**
 	 * 
-	 * @param r0 current row
-	 * @param c0 current column
-	 * @param r1 target row
-	 * @param c1 target column
-	 * @return 0 if current is same with target, 2 if target is an Unit, 1 if move was successful, 3 is too far
+	 * @param r0
+	 *            current row
+	 * @param c0
+	 *            current column
+	 * @param r1
+	 *            target row
+	 * @param c1
+	 *            target column
+	 * @return 0 if current is same with target, 2 if target is an Unit, 1 if move
+	 *         was successful, 3 is too far
 	 */
-	public int move(int r0, int c0, int r1, int c1)
-	{
-		if(r0 == r1 && c0 == c1)
+	public int move(int r0, int c0, int r1, int c1) {
+		if (r0 == r1 && c0 == c1)
 			return 0;
-		if(map[r1][c1] instanceof Unit)
+		if (map[r1][c1] instanceof Unit)
 			return 2;
-		//if((Math.abs(r0-r1) + Math.abs(c0-c1)) > map[r0][c0].getiMobility())
-		if(map[r0][c0] != null && map[r0][c0].getUnitName().equals("Ghost"))
-		{
-			if((Math.abs(r0-r1) + Math.abs(c0-c1)) <= map[r0][c0].getiMobility())
-			{
+		// if((Math.abs(r0-r1) + Math.abs(c0-c1)) > map[r0][c0].getiMobility())
+		if (map[r0][c0] != null && map[r0][c0].getUnitName().equals("Ghost")) {
+			if ((Math.abs(r0 - r1) + Math.abs(c0 - c1)) <= map[r0][c0].getiMobility()) {
 				map[r1][c1] = map[r0][c0];
 				map[r0][c0] = null;
 				return 1;
 			}
 		}
-		if(map[r0][c0] != null && moveable(map,r0,c0,map[r0][c0].getiMobility())[r1][c1] == false)
+		if (map[r0][c0] != null && moveable(map, r0, c0, map[r0][c0].getiMobility())[r1][c1] == false)
 			return 3;
 		map[r1][c1] = map[r0][c0];
 		map[r0][c0] = null;
 		return 1;
 	}
-	
+
 	/**
 	 * 
 	 * @return number of rows
 	 */
-	public int getRow()
-	{
+	public int getRow() {
 		return map.length;
 	}
-	
+
 	/**
 	 * 
 	 * @return number of columns
 	 */
-	public int getCol()
-	{
+	public int getCol() {
 		return map[0].length;
 	}
-	
+
 	/**
 	 * load level
-	 * @param level the level to load
+	 * 
+	 * @param level
+	 *            the level to load
 	 */
-	public void loadMap(int level)
-	{
-		if(level == 1)
+	public void loadMap(int level) {
+		if (level == 1)
 			loadLevel1();
-		if(level == 2)
+		if (level == 2)
 			loadLevel2();
-		if(level == 3)
+		if (level == 3)
 			loadLevel3();
-		if(level == 4)
+		if (level == 4)
 			loadLevel4();
-		if(level == 5)
+		if (level == 5)
 			loadLevel5();
 	}
-	
+
 	/**
 	 * load level 1
 	 */
-	public void loadLevel1()
-	{
-		int r=0;
+	public void loadLevel1() {
+		int r = 0;
 		int enemyCount = 0;
 		String line;
 		Scanner s;
 		try {
 			s = new Scanner(new File("maze1.txt"));
-			while(s.hasNextLine())
-			{
+			while (s.hasNextLine()) {
 				line = s.nextLine();
-				for(int i = 0; i < 16; i++)
-				{
-					if(line.charAt(i) == '1')
+				for (int i = 0; i < 16; i++) {
+					if (line.charAt(i) == '1')
 						map[r][i] = new Wall();
 				}
 				r++;
-			}			
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for(int i = 0; i < map.length; i++)
-		{
-			for(int j = 0; j < map[0].length; j++)
-			{
-				//if(i == 6 || i == 4)
-					//map[i][j] = new UZEnemy0();
-				if(j == 0 && i == 0 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				// if(i == 6 || i == 4)
+				// map[i][j] = new UZEnemy0();
+				if (j == 0 && i == 0 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(0);
 					CharacterSelectController.charList.get(0).fullHeal();
-				}
-				else if(j == 1 && i == 0 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
+				} else if (j == 1 && i == 0 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(1);
 					CharacterSelectController.charList.get(1).fullHeal();
 
-				}
-				else if(j == 2 && i == 0 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
+				} else if (j == 2 && i == 0 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(2);
 					CharacterSelectController.charList.get(2).fullHeal();
-				}
-				else if(enemyCount <= maxEnemies && map[i][j] == null)
-				{
-					if(Math.random() <= 0.15)
-					{
+				} else if (enemyCount <= maxEnemies && map[i][j] == null) {
+					if (Math.random() <= 0.15) {
 						map[i][j] = new UZEnemy0();
 						enemyCount++;
 						if (MainMenuController.mediumDiff == true) {
@@ -196,56 +189,46 @@ public class Map
 		}
 
 	}
-	
+
 	/**
 	 * load level 2
 	 */
-	public void loadLevel2()
-	{
-		int r=0;
+	public void loadLevel2() {
+		int r = 0;
 		int enemyCount = 0;
 		String line;
 		Scanner s;
 		try {
 			s = new Scanner(new File("maze2.txt"));
-			while(s.hasNextLine())
-			{
+			while (s.hasNextLine()) {
 				line = s.nextLine();
-				for(int i = 0; i < 16; i++)
-				{
-					if(line.charAt(i) == '1')
+				for (int i = 0; i < 16; i++) {
+					if (line.charAt(i) == '1')
 						map[r][i] = new Wall();
 				}
 				r++;
-			}			
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for(int i = 0; i < map.length; i++)
-		{
-			for(int j = 0; j < map[0].length; j++)
-			{
-				//if(i == 6 || i == 4)
-					//map[i][j] = new UZEnemy0();
-				if(j == 0 && i == 10 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				// if(i == 6 || i == 4)
+				// map[i][j] = new UZEnemy0();
+				if (j == 0 && i == 10 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(0);
 					CharacterSelectController.charList.get(0).fullHeal();
 					CharacterSelectController.charList.get(0).iLevelUp();
-				}
-				else if(j == 0 && i == 11 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
+				} else if (j == 0 && i == 11 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(1);
 					CharacterSelectController.charList.get(1).fullHeal();
 					CharacterSelectController.charList.get(1).iLevelUp();
-				}
-				else if(j == 1 && i == 11 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
+				} else if (j == 1 && i == 11 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(2);
 					CharacterSelectController.charList.get(2).fullHeal();
 					CharacterSelectController.charList.get(2).iLevelUp();
-				}
-				else if(enemyCount <= maxEnemies && map[i][j] == null)
-				{
-					if(Math.random() <= 0.15)
-					{
+				} else if (enemyCount <= maxEnemies && map[i][j] == null) {
+					if (Math.random() <= 0.15) {
 						map[i][j] = new UZEnemy0();
 						enemyCount++;
 						if (MainMenuController.mediumDiff == true) {
@@ -259,62 +242,52 @@ public class Map
 							map[i][j].iDefenseUp(20);
 						}
 					}
-					
+
 				}
 			}
 		}
 
 	}
-	
+
 	/**
 	 * load level 3
 	 */
-	public void loadLevel3()
-	{
-		int r=0;
+	public void loadLevel3() {
+		int r = 0;
 		int enemyCount = 0;
 		String line;
 		Scanner s;
 		try {
 			s = new Scanner(new File("maze3.txt"));
-			while(s.hasNextLine())
-			{
+			while (s.hasNextLine()) {
 				line = s.nextLine();
-				for(int i = 0; i < 16; i++)
-				{
-					if(line.charAt(i) == '1')
+				for (int i = 0; i < 16; i++) {
+					if (line.charAt(i) == '1')
 						map[r][i] = new Wall();
 				}
 				r++;
-			}			
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for(int i = 0; i < map.length; i++)
-		{
-			for(int j = 0; j < map[0].length; j++)
-			{
-				//if(i == 6 || i == 4)
-					//map[i][j] = new UZEnemy0();
-				if(j == 0 && i == 0 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				// if(i == 6 || i == 4)
+				// map[i][j] = new UZEnemy0();
+				if (j == 0 && i == 0 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(0);
 					CharacterSelectController.charList.get(0).fullHeal();
 					CharacterSelectController.charList.get(0).iLevelUp();
-				}
-				else if(j == 1 && i == 0 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
+				} else if (j == 1 && i == 0 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(1);
 					CharacterSelectController.charList.get(1).fullHeal();
 					CharacterSelectController.charList.get(1).iLevelUp();
-				}
-				else if(j == 0 && i == 1 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
+				} else if (j == 0 && i == 1 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(2);
 					CharacterSelectController.charList.get(2).fullHeal();
 					CharacterSelectController.charList.get(2).iLevelUp();
-				}
-				else if(enemyCount <= maxEnemies && map[i][j] == null)
-				{
-					if(Math.random() <= 0.15)
-					{
+				} else if (enemyCount <= maxEnemies && map[i][j] == null) {
+					if (Math.random() <= 0.15) {
 						map[i][j] = new UZEnemy0();
 						enemyCount++;
 						if (MainMenuController.mediumDiff == true) {
@@ -335,56 +308,46 @@ public class Map
 		}
 
 	}
-	
+
 	/**
 	 * load level 4
 	 */
-	public void loadLevel4()
-	{
-		int r=0;
+	public void loadLevel4() {
+		int r = 0;
 		int enemyCount = 0;
 		String line;
 		Scanner s;
 		try {
 			s = new Scanner(new File("maze4.txt"));
-			while(s.hasNextLine())
-			{
+			while (s.hasNextLine()) {
 				line = s.nextLine();
-				for(int i = 0; i < 16; i++)
-				{
-					if(line.charAt(i) == '1')
+				for (int i = 0; i < 16; i++) {
+					if (line.charAt(i) == '1')
 						map[r][i] = new Wall();
 				}
 				r++;
-			}			
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for(int i = 0; i < map.length; i++)
-		{
-			for(int j = 0; j < map[0].length; j++)
-			{
-				//if(i == 6 || i == 4)
-					//map[i][j] = new UZEnemy0();
-				if(j == 0 && i == 10 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				// if(i == 6 || i == 4)
+				// map[i][j] = new UZEnemy0();
+				if (j == 0 && i == 10 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(0);
 					CharacterSelectController.charList.get(0).fullHeal();
 					CharacterSelectController.charList.get(0).iLevelUp();
-				}
-				else if(j == 0 && i == 11 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
+				} else if (j == 0 && i == 11 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(1);
 					CharacterSelectController.charList.get(1).fullHeal();
 					CharacterSelectController.charList.get(1).iLevelUp();
-				}
-				else if(j == 1 && i == 11 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
+				} else if (j == 1 && i == 11 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(2);
 					CharacterSelectController.charList.get(2).fullHeal();
 					CharacterSelectController.charList.get(2).iLevelUp();
-				}
-				else if(enemyCount <= maxEnemies && map[i][j] == null)
-				{
-					if(Math.random() <= 0.15)
-					{
+				} else if (enemyCount <= maxEnemies && map[i][j] == null) {
+					if (Math.random() <= 0.15) {
 						map[i][j] = new UZEnemy0();
 						enemyCount++;
 						if (MainMenuController.mediumDiff == true) {
@@ -398,63 +361,53 @@ public class Map
 							map[i][j].iDefenseUp(20);//
 						}
 					}
-					
+
 				}
 			}
 		}
 
 	}
-	
+
 	/**
 	 * load level 5
 	 */
-	public void loadLevel5()
-	{
+	public void loadLevel5() {
 		//
-		int r=0;
+		int r = 0;
 		int enemyCount = 0;
 		String line;
 		Scanner s;
 		try {
 			s = new Scanner(new File("maze5.txt"));
-			while(s.hasNextLine())
-			{
+			while (s.hasNextLine()) {
 				line = s.nextLine();
-				for(int i = 0; i < 16; i++)
-				{
-					if(line.charAt(i) == '1')
+				for (int i = 0; i < 16; i++) {
+					if (line.charAt(i) == '1')
 						map[r][i] = new Wall();
 				}
 				r++;
-			}			
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for(int i = 0; i < map.length; i++)
-		{
-			for(int j = 0; j < map[0].length; j++)
-			{
-				//if(i == 6 || i == 4)
-					//map[i][j] = new UZEnemy0();
-				if(j == 0 && i == 10 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[0].length; j++) {
+				// if(i == 6 || i == 4)
+				// map[i][j] = new UZEnemy0();
+				if (j == 0 && i == 10 && (CharacterSelectController.charList.get(0).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(0);
 					CharacterSelectController.charList.get(0).fullHeal();
 					CharacterSelectController.charList.get(0).iLevelUp();
-				}
-				else if(j == 0 && i == 11 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
+				} else if (j == 0 && i == 11 && (CharacterSelectController.charList.get(1).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(1);
 					CharacterSelectController.charList.get(1).fullHeal();
 					CharacterSelectController.charList.get(1).iLevelUp();
-				}
-				else if(j == 1 && i == 11 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
+				} else if (j == 1 && i == 11 && (CharacterSelectController.charList.get(2).getiCurrHitPoints() > 0)) {
 					map[i][j] = CharacterSelectController.charList.get(2);
 					CharacterSelectController.charList.get(2).fullHeal();
 					CharacterSelectController.charList.get(2).iLevelUp();
-				}
-				else if(enemyCount <= maxEnemies && map[i][j] == null)
-				{
-					if(Math.random() <= 0.15)
-					{
+				} else if (enemyCount <= maxEnemies && map[i][j] == null) {
+					if (Math.random() <= 0.15) {
 						map[i][j] = new UZEnemy0();
 						enemyCount++;
 						if (MainMenuController.mediumDiff == true) {
@@ -469,72 +422,78 @@ public class Map
 						}
 						map[i][j].iLevelUp();
 					}
-					
+
 				}
 			}
 		}
 
 	}
-	
+
 	/**
 	 * creates a boolean matrix, true if the unit can move to that cell
-	 * @param map map matrix
-	 * @param r row
-	 * @param c column
-	 * @param range movement range of the Unit
+	 * 
+	 * @param map
+	 *            map matrix
+	 * @param r
+	 *            row
+	 * @param c
+	 *            column
+	 * @param range
+	 *            movement range of the Unit
 	 * @return a boolean matrix, true if the unit can move to that cell
 	 */
-	public boolean[][] moveable(Unit[][] map, int r, int c, int range)
-	{
+	public boolean[][] moveable(Unit[][] map, int r, int c, int range) {
 		boolean[][] mat = new boolean[map.length][map[0].length];
 		firstTrue = null;
 		lastTrue = null;
-		for(int i = 0; i < mat.length; i++)
-		{
-			for(int j = 0; j < mat[0].length; j++)
-			{
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[0].length; j++) {
 				mat[i][j] = false;
 			}
 		}
 		moveableRec(map, mat, r, c, range);
-		mat[r][c]=false;
+		mat[r][c] = false;
 		return mat;
 	}
-	
+
 	/**
 	 * recursive function for movable
-	 * @param map map matrix
-	 * @param mat boolean matrix
-	 * @param r row
-	 * @param c column
-	 * @param range movement range of the Unit
+	 * 
+	 * @param map
+	 *            map matrix
+	 * @param mat
+	 *            boolean matrix
+	 * @param r
+	 *            row
+	 * @param c
+	 *            column
+	 * @param range
+	 *            movement range of the Unit
 	 * @return boolean matrix, true if the unit can move to that cell
 	 */
-	public boolean[][] moveableRec(Unit[][] map, boolean[][] mat, int r, int c, int range)
-	{
-		if(range >= 0)
-		{
-			if(firstTrue == null)
-				firstTrue = new Location(this,r,c);
-			else if(firstTrue.getRow() >= r || firstTrue.getCol() >= c)
-				firstTrue = new Location(this,Math.min(firstTrue.getRow(), r),Math.min(firstTrue.getCol(), c));
-			if(lastTrue == null)
-				lastTrue = new Location(this,r,c);
-			else if(lastTrue.getRow() <= r || lastTrue.getCol() <= c)
-				lastTrue = new Location(this,Math.max(lastTrue.getRow(), r),Math.max(lastTrue.getCol(), c));
+	public boolean[][] moveableRec(Unit[][] map, boolean[][] mat, int r, int c, int range) {
+		if (range >= 0) {
+			if (firstTrue == null)
+				firstTrue = new Location(this, r, c);
+			else if (firstTrue.getRow() >= r || firstTrue.getCol() >= c)
+				firstTrue = new Location(this, Math.min(firstTrue.getRow(), r), Math.min(firstTrue.getCol(), c));
+			if (lastTrue == null)
+				lastTrue = new Location(this, r, c);
+			else if (lastTrue.getRow() <= r || lastTrue.getCol() <= c)
+				lastTrue = new Location(this, Math.max(lastTrue.getRow(), r), Math.max(lastTrue.getCol(), c));
 			mat[r][c] = true;
-			if((r+1)<mat.length && (map[r+1][c] == null || map[r+1][c].isbAlly()))
-				moveableRec(map,mat,r+1,c,range-1);
-			if((r-1)>=0 && (map[r-1][c] == null || map[r-1][c].isbAlly()))
-				moveableRec(map,mat,r-1,c,range-1);
-			if((c+1)<mat[0].length && (map[r][c+1] == null || map[r][c+1].isbAlly()))
-				moveableRec(map,mat,r,c+1,range-1);
-			if((c-1)>=0 && (map[r][c-1] == null || map[r][c-1].isbAlly()))
-				moveableRec(map,mat,r,c-1,range-1);
+			if ((r + 1) < mat.length && (map[r + 1][c] == null || map[r + 1][c].isbAlly()))
+				moveableRec(map, mat, r + 1, c, range - 1);
+			if ((r - 1) >= 0 && (map[r - 1][c] == null || map[r - 1][c].isbAlly()))
+				moveableRec(map, mat, r - 1, c, range - 1);
+			if ((c + 1) < mat[0].length && (map[r][c + 1] == null || map[r][c + 1].isbAlly()))
+				moveableRec(map, mat, r, c + 1, range - 1);
+			if ((c - 1) >= 0 && (map[r][c - 1] == null || map[r][c - 1].isbAlly()))
+				moveableRec(map, mat, r, c - 1, range - 1);
 		}
 		return mat;
 	}
-	
+
 	/**
 	 * resets the map too all null
 	 */
